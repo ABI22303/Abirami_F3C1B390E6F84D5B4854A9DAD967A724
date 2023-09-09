0f3b3f749b449be04fee0f890875d884a3100cfd
ABI22303 <144512472+ABI22303@users.noreply.github.com>
2023-09-09 11:03:52 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,16 @@
+def LeapYearCheck(input_year): 
+    if (input_year % 4) == 0: 
+        if (input_year % 100) == 0: 
+            if (input_year % 400) == 0: 
+                return True
+            else: 
+                return False
+        else: 
+             return True
+    else: 
+        return False
+input_year = int(input("Enter the Year to be checked: "))
+if(LeapYearCheck(input_year)): 
+    print("Leap Year") 
+else: 
+    print("Not a Leap Year")
